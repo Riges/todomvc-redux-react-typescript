4767efc8b7a39d359b6f6fe505d8f326814dafd2
Riges <mail@riges.net>
2017-08-27 18:33:03 +0200

    Add Sample Todo list

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,17 @@ import App from './components/App';
 import reduce from './reducers';
 
 const store = createStore(reduce);
+const unsubscribe = store.subscribe(() =>
+  console.log(store.getState().toArray())
+);
+store.dispatch(addTodo('Coder la SPA'));
+store.dispatch(addTodo('Builder la SPA'));
+store.dispatch(addTodo('Deployer la SPA sur Azure Storage'));
+store.dispatch(addTodo('Deployer la SPA sur WebApp'));
+store.dispatch(toggleTodo((store.getState().get(0).id)));
+store.dispatch(toggleTodo((store.getState().get(1).id)));
+
+unsubscribe();
 
 render(
   <Provider store={store}>
